dd66b4c74de9df42abeee13dda253f22c6cdb12d
balasureshkrishnak <balasureshkrishna.kodavali@gmail.com>
2020-07-15 14:00:17 +0530

    initial files saved

---
+++ src/main/java/com/hcl/googlepay/GooglepayApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.googlepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglepayApplication.class, args);
+	}
+
+}
